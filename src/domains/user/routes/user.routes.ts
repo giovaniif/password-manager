633cd023b6c787f1d328520c42d9ea4dc1248cbf
Giovani Farias <riccog.25@gmail.com>
2021-04-21 15:49:44 -0300

    feat: set user verified route

--- src/domains/user/routes/user.routes.ts
+++ src/domains/user/routes/user.routes.ts
@@ -2,10 +2,13 @@ import { Router } from 'express'
 
 import { CreateUserController } from '@domains/user/controllers/CreateUserController'
 import { adaptRoute } from '@shared/infra/http/routes/middlewares/adaptRoute'
+import { SetUserVerifiedController } from '../controllers/SetUserVerifiedController'
 
 const userRouter = Router()
 const createUsers = new CreateUserController()
+const setVerified = new SetUserVerifiedController()
 
 userRouter.post('/', adaptRoute(createUsers))
+userRouter.post('/verify/:id', adaptRoute(setVerified))
 
 export { userRouter }
