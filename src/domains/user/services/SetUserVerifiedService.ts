74a83731fcde1b631db8a026e704e8a0c589f223
Giovani Farias <riccog.25@gmail.com>
2021-04-21 15:46:15 -0300

    feat: set user verified service

---
+++ src/domains/user/services/SetUserVerifiedService.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from 'tsyringe'
+
+import { Either, left, right } from '@shared/Either'
+import { InvalidUserIdError } from '@shared/errors/User'
+import { IUsersRepository } from '../repositories/IUsersRepository'
+import { User } from '../models/User'
+
+type IResponse = Either<InvalidUserIdError | Error, User>
+
+@injectable()
+export class SetUserVerifiedService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute(userId: string): Promise<IResponse> {
+    if (!userId) return left(new InvalidUserIdError())
+
+    const userOrError = await this.usersRepository.findById(userId)
+    if (userOrError.isLeft()) return left(new InvalidUserIdError())
+
+    try {
+      const verifiedUser = await this.usersRepository.setVerified(userId)
+
+      return right(verifiedUser)
+    } catch (err) {
+      return left(new Error(err.message))
+    }
+  }
+}
